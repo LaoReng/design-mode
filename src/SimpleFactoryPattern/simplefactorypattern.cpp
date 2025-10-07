c6eb0cffa73dd8618c77e145557cdda70203c5af
luweidong <3081977515@qq.com>
2025-10-07 23:32:58 +0800

    添加简单工厂模式例程

---
+++ src/SimpleFactoryPattern/simplefactorypattern.cpp
@@ -0,0 +1,31 @@
+﻿#include "simplefactorypattern.h"
+
+OperationFactory::OperationFactory(/* args */)
+{
+}
+
+OperationFactory::~OperationFactory()
+{
+}
+
+Operation *OperationFactory::CreateOperation(std::string strOperation)
+{
+    Operation *op = nullptr;
+    if (strOperation == "+")
+    {
+        op = new OperationAdd;
+    }
+    else if (strOperation == "-")
+    {
+        op = new OperationSub;
+    }
+    else if (strOperation == "*")
+    {
+        op = new OperationMult;
+    }
+    else if (strOperation == "/")
+    {
+        op = new OperationDiv;
+    }
+    return op;
+}
---
+++ src/SimpleFactoryPattern/simplefactorypattern.h
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+class Operation;
+
+/// @brief 运算工厂
+class OperationFactory
+{
+private:
+    /* data */
+public:
+    OperationFactory(/* args */);
+    ~OperationFactory();
+    Operation *CreateOperation(std::string strOperation);
+};
+
+class Operation
+{
+protected:
+    /* data */
+    double numberA{};
+    double numberB{};
+
+public:
+    Operation(/* args */) {}
+    ~Operation() {}
+    virtual double GetResult() = 0;
+};
+/// @brief 加法运算
+class OperationAdd : public Operation
+{
+public:
+    double GetResult() override
+    {
+        return numberA + numberB;
+    }
+
+private:
+};
+/// @brief 减法运算
+class OperationSub : public Operation
+{
+public:
+    double GetResult() override
+    {
+        return numberA - numberB;
+    }
+};
+/// @brief 乘法运算
+class OperationMult : public Operation
+{
+public:
+    double GetResult() override
+    {
+        return numberA * numberB;
+    }
+};
+/// @brief 除法运算
+class OperationDiv : public Operation
+{
+public:
+    double GetResult() override
+    {
+        if (numberB == 0)
+            return 0;
+        return numberA / numberB;
+    }
+};
