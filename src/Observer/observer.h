56dd37d9288a6af5cd55aab54bdff7b9c504080d
luweidong <3081977515@qq.com>
2025-10-23 19:37:08 +0800

    优化发布者订阅者模式例程支持添加不同函数名的订阅者

--- src/Observer/observer.h
+++ src/Observer/observer.h
@@ -1,17 +1,45 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <functional>
 /// @brief 订阅者基类
 class Subscribe
 {
 public:
     virtual void Update() = 0;
 };
+/// @brief 函数绑定器
+class CBinder
+{
+public:
+    template <typename _OBJ, typename _FUNC>
+    CBinder(_OBJ *sub, _FUNC func)
+        : m_func(std::bind(func, sub)), m_pObj(sub), m_pFunc(*(void **)&func)
+    {
+    }
+    void operator()()
+    {
+        m_func();
+    }
+    bool operator==(const CBinder &other)
+    {
+        if (m_pObj == other.m_pObj && m_pFunc == other.m_pFunc)
+            return true;
+        return false;
+    }
+
+private:
+    std::function<void()> m_func; // 函数包装器
+    void *m_pObj{};               // this指针 可以为空
+    void *m_pFunc{};              // 函数指针
+};
+
 /// @brief 发布者基类
 class Publish
 {
-private:
+protected:
     std::list<Subscribe *> m_lstSubscribe;
+    std::list<CBinder *> m_lstSubs;
 
 public:
     /// @brief 添加订阅者
@@ -20,16 +48,50 @@ public:
     {
         m_lstSubscribe.push_back(sub);
     }
+    /// @brief 添加订阅者
+    /// @tparam _OBJ 订阅者类型
+    /// @tparam _FUNC 订阅者触发成员函数类型
+    /// @param sub 订阅者
+    /// @param func 发布者所触发的订阅者对象的函数
+    template <typename _OBJ, typename _FUNC>
+    void Add(_OBJ *sub, _FUNC func)
+    {
+        m_lstSubs.push_back(new CBinder(sub, func));
+    }
     /// @brief 移除订阅者
     /// @param sub 订阅者
     virtual void Remove(Subscribe *sub)
     {
+        if (sub == nullptr)
+        {
+            std::cout << "bind调用" << std::endl;
+            return;
+        }
         auto it = std::find(m_lstSubscribe.begin(), m_lstSubscribe.end(), sub);
         if (it != m_lstSubscribe.end())
         {
             m_lstSubscribe.erase(it);
         }
     }
+    /// @brief 移除订阅者
+    /// @tparam _OBJ 订阅者类型
+    /// @tparam _FUNC 订阅者触发成员函数类型
+    /// @param sub 订阅者
+    /// @param func 发布者所触发的订阅者对象的函数
+    template <typename _OBJ, typename _FUNC>
+    void Remove(_OBJ *sub, _FUNC func)
+    {
+        CBinder bindData(sub, func);
+        for (auto it = m_lstSubs.begin(); it != m_lstSubs.end(); it++)
+        {
+            if (**it == bindData)
+            {
+                m_lstSubs.erase(it);
+                break;
+            }
+        }
+    }
+
     virtual void Nodify()
     {
         for (auto it : m_lstSubscribe)
@@ -49,8 +111,20 @@ public:
     {
         return m_name;
     }
+    virtual void Nodify() override
+    {
+        if (m_lstSubs.empty())
+        {
+            Publish::Nodify();
+            return;
+        }
+        for (auto it : m_lstSubs)
+        {
+            (*it)();
+        }
+    }
 };
-/// @brief 具体订阅者牛马类型小张
+/// @brief 具体订阅者牛马类型
 class Effort : public Subscribe
 {
 private:
@@ -64,6 +138,11 @@ public:
     virtual void Update()
     {
         std::cout << m_boss->GetName() << "来了，继续工作！！！\n";
+    }
+    int Test1(int a)
+    {
+        std::cout << a;
+        return 0;
     }
 };
 /// @brief 具体订阅者摆烂类型
@@ -80,5 +159,37 @@ public:
     virtual void Update()
     {
         std::cout << m_boss->GetName() << "来了，快去请假！！！\n";
+    }
+};
+/// @brief 具体订阅者牛马类型[初步优化(实现支持不同函数名的调用)]
+class Effort2
+{
+private:
+    Boss *m_boss{};
+
+public:
+    Effort2(Boss *boss)
+        : m_boss(boss)
+    {
+    }
+    void Effort2Update()
+    {
+        std::cout << "[" << __FUNCTION__ << "]:" << m_boss->GetName() << "来了，继续工作！！！\n";
+    }
+};
+/// @brief 具体订阅者摆烂类型[初步优化(实现支持不同函数名的调用)]
+class Ease2
+{
+private:
+    Boss *m_boss{};
+
+public:
+    Ease2(Boss *boss)
+        : m_boss(boss)
+    {
+    }
+    void Ease2Update()
+    {
+        std::cout << "[" << __FUNCTION__ << "]:" << m_boss->GetName() << "来了，快去请假！！！\n";
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,45 @@
 #include "Observer/observer.h"
 #endif // OBSERVER
 
+int test1(int  a, int  b)
+{
+
+    return 1-b-a;
+}
+int test2(int a, int b)
+{
+    std::cout << "nihao\n";
+    return 0;
+}
+
+typedef int(*TEST)(int, int);
+
 int main()
 {
+    test1(1,2);
+    test2(2,3);
+    std::function<int(int, int)> t1 = test1;
+    std::function<int(int, int)> t2 = test2;
+
+    void* t1Ptr = (void*)&test1;
+    TEST Test1 = *(TEST*)&t1Ptr;
+    Test1(1, 2);
+    void* t2Ptr = (void*)&test2;
+
+    std::cout << t1Ptr << std::endl << t2Ptr << std::endl;
+    if (t1Ptr == t2Ptr) {
+        std::cout << "相等\n";
+    }
+    else {
+        std::cout << "不相等\n";
+    }
+    //return 0;
+
 #ifdef DECORATOR
-    Person* per = new Person("Mr.Lu");
-    SuitPants* sp = new SuitPants;
-    SuitJacket* sj = new SuitJacket;
-    Sneakers* sk = new Sneakers;
+    Person *per = new Person("Mr.Lu");
+    SuitPants *sp = new SuitPants;
+    SuitJacket *sj = new SuitJacket;
+    Sneakers *sk = new Sneakers;
 
     sp->Decorator(per);
     sj->Decorator(sp);
@@ -44,21 +76,21 @@ int main()
     proxy.GiveSnacks();
 #endif // PROXY
 #ifdef FACTORY_METHOD
-    OperationFactoryMethod* ofm = new FactoryMethodSub;
-    OperationBase* ob = ofm->CreateOperation();
+    OperationFactoryMethod *ofm = new FactoryMethodSub;
+    OperationBase *ob = ofm->CreateOperation();
     ob->m_optNum1 = 1;
     ob->m_optNum2 = 2;
     std::cout << ob->m_optNum1 << " - " << ob->m_optNum2 << " = " << ob->GetResult() << std::endl;
 #endif // FACTORY_METHOD
 #ifdef PROTOTYPE
-    CVitae* lu = new CVitae;
+    CVitae *lu = new CVitae;
     lu->SetName("大鸟");
     lu->SetAge(21);
     lu->SetWorkExp1("2024/7-2024/12", "Neusoft");
     lu->SetWorkExp2("2025/3-2025/6", "zhiyuan");
-    CVitae* a = (CVitae*)lu->Clone();
+    CVitae *a = (CVitae *)lu->Clone();
     a->SetWorkExp2("2025/3-2025/6", "ZY");
-    CVitae* b = (CVitae*)lu->Clone();
+    CVitae *b = (CVitae *)lu->Clone();
     b->SetName("小菜");
     lu->Display();
     a->Display();
@@ -81,7 +113,7 @@ int main()
     facade.MethodB();
 #endif // FACADE
 #ifdef BUILDER
-    Builder* pBuilderA = new TranslateBuilderA;
+    Builder *pBuilderA = new TranslateBuilderA;
     Director d(pBuilderA);
     d.Create();
 #endif // BUILDER
@@ -95,8 +127,28 @@ int main()
     std::cout << "========华丽分割线========\n";
     laoLu.Remove(&xiaoLi);
     laoLu.Nodify();
-#endif // OBSERVER
+    std::cout << "========华丽分割线========\n";
+    Effort2 xw(&laoLu);
+    Ease2 xs(&laoLu);
+    Ease2 xx(&laoLu);
+    laoLu.Add(&xw, &Effort2::Effort2Update);
+    laoLu.Add(&xs, &Ease2::Ease2Update);    // 消息订阅不能为函数重载
+    laoLu.Add(&xx, &Ease2::Ease2Update);
+    laoLu.Nodify();
+    laoLu.Remove(&xx, &Ease2::Ease2Update);
+    std::cout << "========华丽分割线========\n";
+    laoLu.Nodify();
 
+    //void* ptrPtr = Effort2::Effort2Update;
+    //unsigned aaa = &test1;
+    //std::cout << (void*)&;
+
+    // Effort::Update(&xiaoZhang);
+
+    // connect(&laoLu, &Boss::Add, &xiaoZhang, &Effort::Test1);
+
+    // connect()
+#endif // OBSERVER
 
     return 0;
 }
